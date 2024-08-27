29fbb18505897e2100082532e3392b341ee95a4a
Rui de Klerk <rvzk.mota@gmail.com>
2024-08-27 16:35:40 +0100

    add prototype Button

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,30 @@
+import React, { MouseEventHandler } from 'react'
+import { Button as Btn } from 'antd';
+
+export type ButtonProps = {
+  text?: string;
+  type?: "link" | "text" | "primary" | "default" | "dashed" | undefined;
+  disabled?: boolean;
+  size?: "small" | "medium" | "large";
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+};
+
+const Button: React.FC<ButtonProps> = ({
+  type='default',
+  disabled=false,
+  text,
+  onClick,
+  ...props
+}) => {
+  return (
+    <Btn
+      type={type}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {text}
+    </Btn>
+  )
+}
+
+export default Button
