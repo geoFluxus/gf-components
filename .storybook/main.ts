5887af98234b7b2f31412e307cd25e057cd9489c
Rui de Klerk <rvzk.mota@gmail.com>
2024-08-28 09:46:30 +0100

    minor adjustments

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,11 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/__docs__/*.stories.tsx", "../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    "../src/**/__docs__/*.stories.tsx", 
+    "../src/**/*.mdx", 
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
