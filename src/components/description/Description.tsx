e33262b9d574fb6127ae8de260217db3bffae189
Rui de Klerk <rvzk.mota@gmail.com>
2024-09-19 12:33:23 +0100

    refactor Description with styled-components

--- src/components/description/Description.tsx
+++ src/components/description/Description.tsx
@@ -1,7 +1,7 @@
 import React, { CSSProperties, useState } from 'react'
 import { Typography } from 'antd';
-import classNames from 'classnames';
-import './Description.css'
+import GlobalStyle from '../../globalStyles';
+import styled from 'styled-components';
 
 const { Text } = Typography
 export interface Props {
@@ -11,6 +11,14 @@ export interface Props {
   style?: CSSProperties | undefined;
   className?: string;
 }
+
+const StyledDiv = styled.div<{columns?: 2 | 3 | 4 }>`
+  column-count: ${props => props.columns}
+`
+const StyledText = styled(Text)`
+  font: var(--gf-label-lg-default);
+  color: var(--gf-color-text-secondary) ;
+`
 
 const Description: React.FC<Props> = ({
   text,
@@ -22,15 +30,13 @@ const Description: React.FC<Props> = ({
   const [ editableText, setEditableText ] = useState(text)
   const edit = editable ? { onChange: setEditableText } : false
 
-  const classes = classNames(
-    `gf-description-${columns}`,
-    className
-  )
-
   return (
-    <div className={classes} style={style || {}}>
-      <Text className='gf-description-body' editable={edit}>{editableText}</Text>
-    </div>
+    <>
+      <GlobalStyle />
+      <StyledDiv columns={columns} style={style || {}} className={className || ''}>
+        <StyledText editable={edit}>{editableText}</StyledText>
+      </StyledDiv>
+    </>
   )
 }
 
