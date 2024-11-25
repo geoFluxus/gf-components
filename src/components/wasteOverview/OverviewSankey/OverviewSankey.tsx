63b7cf16f31b695bc57c1c8a69ed9f89bba20662
vasilis <vasilis.bouzas@geofluxus.com>
2024-11-25 09:40:42 +0100

    0.1.61

--- src/components/wasteOverview/OverviewSankey/OverviewSankey.tsx
+++ src/components/wasteOverview/OverviewSankey/OverviewSankey.tsx
@@ -7,13 +7,11 @@ import { CustomToolTip } from "../../customToolTip";
 const OverviewSankey = ({
     data,
     width='100%',
-    height='100%'
 }) => {
     return (
         <>
             <svg
                 width={width}
-                height={height}
                 viewBox="0 0 894 568"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
