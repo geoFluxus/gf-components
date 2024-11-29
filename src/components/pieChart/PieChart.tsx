6ab810f27c7fca3ef3e7eb620402082b7a33a760
Gaurangi Mane <manegaurangi89@gmail.com>
2024-11-29 11:10:43 +0530

    Update piechart component for text align and reducing chart height

--- src/components/pieChart/PieChart.tsx
+++ src/components/pieChart/PieChart.tsx
@@ -3,12 +3,13 @@ import GlobalStyle from "../../globalStyles";
 import { ResponsivePie } from "@nivo/pie";
 import styled from "styled-components";
 import { CustomToolTip } from "../customToolTip";
+import { Col, Row } from "antd";
 
 export interface Props {
   pieChartData: any;
   title: string;
   isEmpty: boolean;
-  tooltip: ({ id, value, color }) => JSX.Element;
+  tooltip?: ({ id, value, unit }) => JSX.Element;
 }
 
 const StyledSpan = styled.span`
@@ -48,67 +49,75 @@ const PieChart: React.FC<Props> = ({
   return (
     <>
       <GlobalStyle />
-      <div style={{ width: "100%", height: 600, position: "relative" }}>
-        <StyledSpan>{title}</StyledSpan>
-
-        <ResponsivePie
-          colors={{ scheme: isEmpty ? "greys" : "nivo" }}
-          data={pieChartData}
-          enableArcLabels={!isEmpty}
-          enableArcLinkLabels={!isEmpty}
-          margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-          innerRadius={0.5}
-          padAngle={0.7}
-          cornerRadius={3}
-          activeOuterRadiusOffset={8}
-          borderWidth={1}
-          borderColor={{ from: "color", modifiers: [["darker", 0.2]] }}
-          arcLinkLabelsSkipAngle={10}
-          arcLinkLabelsTextColor="#333333"
-          arcLinkLabelsThickness={2}
-          arcLinkLabelsColor={{ from: "color" }}
-          arcLabelsSkipAngle={10}
-          arcLabelsTextColor={{ from: "color", modifiers: [["darker", 2]] }}
-          isInteractive={!isEmpty}
-          layers={[
-            "arcs",
-            "arcLabels",
-            "arcLinkLabels",
-            "legends",
-            CenteredMetric,
-          ]}
-          defs={[
-            {
-              id: "dots",
-              type: "patternDots",
-              background: "inherit",
-              color: "rgba(255, 255, 255, 0.3)",
-              size: 4,
-              padding: 1,
-              stagger: true,
-            },
-            {
-              id: "lines",
-              type: "patternLines",
-              background: "inherit",
-              color: "rgba(255, 255, 255, 0.3)",
-              rotation: -45,
-              lineWidth: 6,
-              spacing: 10,
-            },
-          ]}
-          tooltip={({ datum: { id, value, color } }) => {
-            return (
-              <CustomToolTip
-                body={
-                  tooltip?.({ id, value, color }) || (
-                    <span>Pie chart tooltip</span>
-                  )
-                }
-              />
-            );
-          }}
-        />
+      <div style={{ width: "100%", height: 500, position: "relative" }}>
+        <Col>
+          <Row justify="center">
+            <StyledSpan style={{ marginLeft: "0px" }}>{title}</StyledSpan>
+          </Row>
+          <Row
+            style={{ width: "100%", height: 500, position: "relative" }}
+            justify="center"
+          >
+            <ResponsivePie
+              colors={{ scheme: isEmpty ? "greys" : "nivo" }}
+              data={pieChartData}
+              enableArcLabels={!isEmpty}
+              enableArcLinkLabels={!isEmpty}
+              margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+              innerRadius={0.5}
+              padAngle={0.7}
+              cornerRadius={3}
+              activeOuterRadiusOffset={8}
+              borderWidth={1}
+              borderColor={{ from: "color", modifiers: [["darker", 0.2]] }}
+              arcLinkLabelsSkipAngle={10}
+              arcLinkLabelsTextColor="#333333"
+              arcLinkLabelsThickness={2}
+              arcLinkLabelsColor={{ from: "color" }}
+              arcLabelsSkipAngle={10}
+              arcLabelsTextColor={{ from: "color", modifiers: [["darker", 2]] }}
+              isInteractive={!isEmpty}
+              layers={[
+                "arcs",
+                "arcLabels",
+                "arcLinkLabels",
+                "legends",
+                CenteredMetric,
+              ]}
+              defs={[
+                {
+                  id: "dots",
+                  type: "patternDots",
+                  background: "inherit",
+                  color: "rgba(255, 255, 255, 0.3)",
+                  size: 4,
+                  padding: 1,
+                  stagger: true,
+                },
+                {
+                  id: "lines",
+                  type: "patternLines",
+                  background: "inherit",
+                  color: "rgba(255, 255, 255, 0.3)",
+                  rotation: -45,
+                  lineWidth: 6,
+                  spacing: 10,
+                },
+              ]}
+              tooltip={({ datum: { id, value, unit } }) => {
+                return (
+                  <CustomToolTip
+                    body={
+                      tooltip?.({ id, value, unit }) || (
+                        <span>Pie chart tooltip</span>
+                      )
+                    }
+                  />
+                );
+              }}
+            />
+          </Row>
+        </Col>
       </div>
     </>
   );
