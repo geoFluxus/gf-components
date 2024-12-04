6105004544eafd89f0d97ce59f76fe9f13abafba
vasilis <vasilis.bouzas@geofluxus.com>
2024-12-04 14:44:17 +0100

    0.1.79

--- src/components/barChart/BarChart.tsx
+++ src/components/barChart/BarChart.tsx
@@ -1,8 +1,8 @@
 import { useRef, useEffect, useState } from "react";
 import { ResponsiveBar } from '@nivo/bar'
+import { CustomToolTip } from "../customToolTip";
 import styled from 'styled-components';
-import { CustomToolTip } from "../../customToolTip";
-import { flows } from "../flows"
+import { Flex } from 'antd'
 
 
 const fontSize = 12
@@ -11,6 +11,13 @@ const StyledText = styled.tspan`
   font: var(--gf-label-md-default);
   color: var(--gf-color-text-secondary);
   font-size: ${fontSize}px;
+  line-height: ${lineHeight}px;
+`
+
+const StyledLabel = styled.span`
+  font: var(--gf-label-md-default);
+  color: var(--gf-color-text-secondary);
+  font-size: 10px;
   line-height: ${lineHeight}px;
 `
 
@@ -39,25 +46,24 @@ const wrapText = (text, width, fontSize=12) => {
 };
 
 
-const OverviewBarchart = ({
-    data=null,
+const BarChart = ({
+    data=[],
     height=600,
     margin={},
-    labelWidth=120,
-    labelPadding=20,
+    padding=0.5,
     keys=null,
     indexBy=null,
-    tooltip=null
+    tooltip=null,
+    labelWidth=120,
+    labelPadding=20,
+    axisBottom={}
 }) => {
     const CustomNode = ({ bar }) => {
         const gRef = useRef(null);
         const [gHeight, setGHeight] = useState(0);
 
         // wrap long text
-        const data = bar.data.data
-        const numeral = flows?.[data?.[indexBy]]?.key
-        const name = flows?.[data?.[indexBy]]?.name
-        const text = `${numeral}. ${name}`
+        const text = bar?.data?.indexValue || 'label'
         const lines = wrapText(text, labelWidth, fontSize);
 
         // track g height
@@ -71,7 +77,7 @@ const OverviewBarchart = ({
         // positioning
         const transX = labelWidth + labelPadding
         const transY = bar.y + bar.height / 2 - gHeight / 2
-        
+
         return (
             <g ref={gRef} transform={`translate(-${transX}, ${transY})`} >
                 <text
@@ -83,7 +89,7 @@ const OverviewBarchart = ({
                     }}
                 >
                     {lines.map((line, index) => (
-                      <StyledText key={index} x={index == 0 ? 0 : (numeral < 10) ? 14 : 20}
+                      <StyledText key={index} x={0}
                         dy={index > 0 ? lineHeight : fontSize}
                       >
                         {line}
@@ -94,7 +100,6 @@ const OverviewBarchart = ({
         )
     }
 
-    // @ts-ignore
     let currName = null
     const CustomNodeLayer = ({ bars }) =>
         bars.map((bar, idx) => {
@@ -104,34 +109,57 @@ const OverviewBarchart = ({
            return (<CustomNode key={`bar-label-${idx}`} bar={bar} />)
         });
 
+
+    const Legend = ({data, keys}) => {
+        const legend = keys?.map(key => ({
+            name: key,
+            color: data[0]?.[`${key}Color`]
+        }))
+
+        return (
+            <Flex gap={16} className='gf-full' justify="center" wrap>
+                {legend.map(l =>
+                    <Flex gap={8} align="center">
+                        <div style={{
+                            minWidth: 16,
+                            minHeight: 16,
+                            backgroundColor: l.color
+                        }} />
+                        <StyledLabel>{l.name}</StyledLabel>
+                    </Flex>
+                )}
+            </Flex>
+        )
+    }
+
     const reverseData = data.slice().reverse()
     return (
-        <div style={{height: height}}>
-            <ResponsiveBar
-                data={reverseData}
-                colors={({ id, data }) => String(data[`${id}Color`])}
-                keys={keys}
-                indexBy={indexBy}
-                margin={{ top: 30, right: 10, bottom: 0, left: 140, ...margin }}
-                layout="horizontal"
-                enableGridY={false}
-                enableGridX={true}
-                padding={0.5}
-                enableLabel={false}
-                axisTop={{
-                    tickSize: 5
-                }}
-                axisBottom={null}
-                axisLeft={null}
-                layers={['grid', 'axes', 'bars', CustomNodeLayer]}
-                tooltip={(bar) => {
-                    return (
-                      <CustomToolTip body={ tooltip?.(bar) || <span>Overview barchart tooltip</span>} />
-                    );
-                }}
-            />
-        </div>
+        <Flex vertical gap={8}>
+            <div style={{height: height}}>
+                <ResponsiveBar
+                    data={reverseData}
+                    colors={({ id, data }) => String(data[`${id}Color`])}
+                    keys={keys}
+                    indexBy={indexBy}
+                    margin={{ top: -10, right: 0, bottom: 50, left: 140, ...margin }}
+                    layout="horizontal"
+                    enableGridY={false}
+                    enableGridX={false}
+                    padding={padding}
+                    enableLabel={false}
+                    axisBottom={axisBottom}
+                    axisLeft={null}
+                    layers={['grid', 'axes', 'bars', CustomNodeLayer]}
+                    tooltip={(bar) => {
+                        return (
+                          <CustomToolTip body={ tooltip?.(bar) || <span>Tooltip</span>} />
+                        );
+                    }}
+                />
+            </div>
+            <Legend data={data} keys={keys}/>
+        </Flex>
     )
 }
 
-export default OverviewBarchart
+export default BarChart
---
+++ src/components/barChart/__docs__/data.js
@@ -0,0 +1,47 @@
+export const data = [
+    {
+        "flow": "Invoer nationaal",
+        "Consumptie huishoudens": 91,
+        "Consumptie huishoudensColor": "hsla(205, 40%, 79%, 1)",
+        "Dienstverlening bedrijven": 113,
+        "Dienstverlening bedrijvenColor": "hsla(83, 47%, 73%, 1)",
+        "Investeringen vaste activa": 20,
+        "Investeringen vaste activaColor": "hsla(4, 55%, 75%, 1)",
+        "Overheid": 170,
+        "OverheidColor": "hsla(35, 74%, 73%, 1)",
+        "Productie goederen": 152,
+        "Productie goederenColor": "hsla(269, 24%, 77%, 1)",
+        "Verandering voorraden": 95,
+        "Verandering voorradenColor": "hsla(26, 74%, 58%, 1)"
+    },
+    {
+        "flow": "Invoer internationaal",
+        "Consumptie huishoudens": 19,
+        "Consumptie huishoudensColor": "hsla(205, 40%, 79%, 1)",
+        "Dienstverlening bedrijven": 31,
+        "Dienstverlening bedrijvenColor": "hsla(83, 47%, 73%, 1)",
+        "Investeringen vaste activa": 200,
+        "Investeringen vaste activaColor": "hsla(4, 55%, 75%, 1)",
+        "Overheid": 17,
+        "OverheidColor": "hsla(35, 74%, 73%, 1)",
+        "Productie goederen": 52,
+        "Productie goederenColor": "hsla(269, 24%, 77%, 1)",
+        "Verandering voorraden": 59,
+        "Verandering voorradenColor": "hsla(26, 74%, 58%, 1)"
+    },
+    {
+        "flow": "Aanbod eigen regio",
+        "Consumptie huishoudens": 61,
+        "Consumptie huishoudensColor": "hsla(205, 40%, 79%, 1)",
+        "Dienstverlening bedrijven": 83,
+        "Dienstverlening bedrijvenColor": "hsla(83, 47%, 73%, 1)",
+        "Investeringen vaste activa": 230,
+        "Investeringen vaste activaColor": "hsla(4, 55%, 75%, 1)",
+        "Overheid": 71,
+        "OverheidColor": "hsla(35, 74%, 73%, 1)",
+        "Productie goederen": 25,
+        "Productie goederenColor": "hsla(269, 24%, 77%, 1)",
+        "Verandering voorraden": 99,
+        "Verandering voorradenColor": "hsla(26, 74%, 58%, 1)"
+    },
+]
