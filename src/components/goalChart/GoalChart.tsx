566e458de18fc20b7418df6d8730f8a2faf1bf1f
vasilis <vasilis.bouzas@geofluxus.com>
2025-01-21 13:29:21 +0100

    0.1.102

--- src/components/goalChart/GoalChart.tsx
+++ src/components/goalChart/GoalChart.tsx
@@ -39,6 +39,10 @@ const GoalChart = ({
     goalData = data?.goal,
     area = data?.area;
   const barColor = colors[goalData ? "national" : "area"];
+  const maxY = Math.max(
+    ...barData.map(b => keys.filter(key => key in b).reduce((acc, key) => acc + b[key], 0)),
+    goalData?.value || 0
+  )
 
     const GoalLayer = (props) =>
         goalData &&
@@ -122,6 +126,8 @@ const GoalChart = ({
                     margin={{left: 50, bottom: 50, right: legendWidth, ...margin}}
                     padding={padding}
                     valueFormat={d => valueFormat?.(d) || d}
+                    minValue={0}
+                    maxValue={maxY}
                     axisBottom={{
                         tickSize: 5,
                         legendPosition: 'middle',
